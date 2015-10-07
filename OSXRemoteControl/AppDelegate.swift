09f04d618e243145d1fa08e4fc9891ae65ea5b52
AJ <ajpriola@gmail.com>
2015-10-06 22:18:31 -0400

    Initial Commit

---
+++ OSXRemoteControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSXRemoteControl
+//
+//  Created by AJ Priola on 10/6/15.
+//  Copyright © 2015 AJ Priola. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSXRemoteControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXRemoteControl
+//
+//  Created by AJ Priola on 10/6/15.
+//  Copyright © 2015 AJ Priola. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
